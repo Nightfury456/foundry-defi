f3e2acde5efe95f40ff8aab147080e964ebba3b8
Nightfury456 <ak7217@srmist.edu.in>
2025-04-06 12:37:29 +0530

    depositAndMint function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -79,7 +79,21 @@ contract DSCEngine {
     ////////////////////////
     // External Functions //
     ////////////////////////
-    function depositCollateralAndMintDsc() external {}
+
+    /*
+     * @param tokenCollateralAddress The address of the token to deposit as collateral
+     * @param amountCollateral The amount of collateral to deposit
+     * @param amountDscToMint The amount of Decentralized stablecoin to mint
+     * @notice this function will deposit your collateral and mint DSC in one transection
+     */
+    function depositCollateralAndMintDsc(
+        address tokenCollateralAddress,
+        uint256 amountCollateral,
+        uint256 amountDscToMint
+    ) external {
+        depositCollateral(tokenCollateralAddress, amountCollateral);
+        mintDsc(amountDscToMint);
+    }
 
     /*
      * @param tokenCollateralAddress The address of the collateral token.
@@ -102,7 +116,7 @@ contract DSCEngine {
 
     function redeemCollateral() external {}
 
-    function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) {
+    function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) {
         s_DSCMinted[msg.sender] += amountDscToMint;
         _revertIfHealthFactorIsBroken(msg.sender);
         bool minted = i_dsc.mint(msg.sender, amountDscToMint);
